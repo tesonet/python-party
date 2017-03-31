77e7ca0c74346f2df928eaba156d2b3bde0f964e
Justinas Kuizinas <justinask7@gmail.com>
2017-03-31 13:27:48 +0300

    From the list of similar soundex codes, finding the most matching one with it's words

--- find.py
+++ find.py
@@ -26,6 +26,47 @@ def input_validation(cli_input):
 
 if input_validation(sys.argv):
     search_word_code = count_soundex(sys.argv[2])
-    print(search_word_code)
+    search_word_number = int(search_word_code[1:])
+    similar_soundex = {}
+    with fileinput.FileInput(sys.path[0] + '/' + sys.argv[1]) as file:
+        for line in file:
+            for word in line.split(' '):
+                if '/' in word:
+                    word = word.split('/')[0]
+                word = word.rstrip('\n')
+                word = word.strip(',')
+                word = word.strip('()')
+                word = word.strip('[]')
+                word_soundex = count_soundex(word)
+                if word_soundex and word_soundex[0] == search_word_code[0]:
+                    if not similar_soundex.get(word_soundex):
+                        similar_soundex[word_soundex] = [word]
+                    else:
+                        similar_soundex[word_soundex].append(word)
+    similar_soundex_keys = [d for d in similar_soundex.keys()]
+    similar_soundex_keys = sorted(similar_soundex_keys)
+    for index, i in enumerate(similar_soundex_keys):
+        similar_soundex_keys[index] = int(i[1:])
+    middle_index = int(len(similar_soundex_keys)/2)
+    difference_from_search = abs(similar_soundex_keys[middle_index] - search_word_number)
+    matched_index = 0
+    if similar_soundex_keys[middle_index] < search_word_number:
+        for l in range(middle_index, len(similar_soundex_keys)):
+            if abs(similar_soundex_keys[l] - search_word_number) < difference_from_search:
+                matched_index = l
+                difference_from_search = abs(similar_soundex_keys[l] - search_word_number)
+            else:
+                break
+    else:
+        for l in reversed(range(0, middle_index)):
+            if abs(similar_soundex_keys[l] - search_word_number) < difference_from_search:
+                matched_index = l
+                difference_from_search = abs(similar_soundex_keys[l] - search_word_number)
+            else:
+                break
+
+    return_list = similar_soundex.get(search_word_code[0] + str(similar_soundex_keys[matched_index]))
+
+
 
 
