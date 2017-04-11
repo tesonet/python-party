777310bce167785dd8143e47359d71fd4f089994
LilyMaster <lilymaster@gmail.com>
2017-04-11 23:52:52 +0300

    Remove Soundex class
    Was not used anywhere.

--- find.py
+++ find.py
@@ -8,32 +8,6 @@ import re
 import click
 
 
-class Soundex(object):
-    """Class for keeping info on one word."""
-
-    def __init__(self, string=''):
-        self.string = string
-
-    @property
-    def rating(self):
-        """Rating of the word"""
-        return len(self.string)
-
-    @staticmethod
-    def drop_letters(string: str, letters: str) -> str:
-        """Drop given letters from string."""
-        dropped = string.translate(
-            {ord(c): None for c in letters}
-        )
-        return dropped
-
-    def drop_aeiouyhw(self, string: str) -> str:
-        """Drop letters that are not required for the Soundex algorithm."""
-        dropped = self.drop_letters(string, 'aeiou' + 'aeiou'.upper())
-        dropped = self.drop_letters(dropped, 'yhw' + 'yhw'.upper())
-        return dropped
-
-
 def drop_letters(string: str, letters: str) -> str:
     """Drop given letters from string."""
     dropped = string.translate(
